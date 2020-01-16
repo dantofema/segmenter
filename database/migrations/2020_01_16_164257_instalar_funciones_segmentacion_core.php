635fb2db1798532f58283197133ef519c50562e8
manureta <manuel@retamozo.com.ar>
2020-01-16 16:16:34 -0300

    Instalar funciones de segmentacion-core

---
+++ database/migrations/2020_01_16_164257_instalar_funciones_segmentacion_core.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class InstalarFuncionesSegmentacionCore extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Eloquent::unguard();
+        DB::statement('CREATE SCHEMA IF NOT EXISTS indec');
+        //$this->command->info('- Instalando funcion de conteo...');
+        $path = 'app/developer_docs/segmentacion-core/lados_completos/cargar_conteos.sql';
+        DB::unprepared(file_get_contents($path));
+        //$this->command->info('Generar Conteos instalado!');
+        //$this->command->info('- Instalando funcion de adyacencias...');
+        $path = 'app/developer_docs/segmentacion-core/lados_completos/generar_adyacencias.sql';
+        DB::unprepared(file_get_contents($path));
+        //$this->command->info('Generar Adyacencias instalado!');
+        //$this->command->info('- Instalando segmentador a mananas independientes (listado)...');
+        $path = 'app/developer_docs/segmentacion-core/manzanas_independientes/segmentar_equilibrado.sql';
+        DB::unprepared(file_get_contents($path));
+        //$this->command->info('Segmantación de manzanas independientes instalado!');
+        echo 'segmentacion-core instalado';
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
