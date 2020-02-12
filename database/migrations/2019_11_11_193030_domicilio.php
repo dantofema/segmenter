6336200ea2144d6668ce584fe78fc0903b104c2b
manureta <manuel@retamozo.com.ar>
2020-02-12 18:36:15 -0300

    Agrego rollback de secuencia

--- database/migrations/2019_11_11_193030_domicilio.php
+++ database/migrations/2019_11_11_193030_domicilio.php
@@ -85,6 +85,7 @@ class Domicilio extends Migration
     public function down()
     {
         //
+     DB::unprepared('DROP SEQUENCE if exists domicilios_id_seq cascade;');
 	 Schema::dropIfExists('domicilios');
     }
 }
