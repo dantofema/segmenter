3ae93bb5d552ada38a6fef373854e30d844b9f9c
Manuel Retamozo <manuel@retamozo.com.ar>
2020-02-20 17:31:40 -0300

    Intentos de ampliar memoria... TODO: error en codificación utf8 de python

--- app/Segmentador.php
+++ app/Segmentador.php
@@ -13,25 +13,34 @@ class Segmentador extends Model
     
     public function segmentar_a_lado_completo($aglo,$prov,$dpto,$frac,$radio,$vivs_deseada,$vivs_max,$vivs_min,$mza_indivisible)
 	{
-
+        // Set the limit to 500 MB.
+        // Varios intentos para aumentar la memoria.
+        /*
+        $fiveMBs = 500 * 1024 * 1024;
+        $fp = fopen("php://temp/maxmemory:$fiveMBs", 'r+');
+        fwrite($fp, 'test');
+        rewind($fp);
+        ini_set('memory_limit','512M');
+        */
         $processLog = Process::fromShellCommandline('echo "Se va a segmentar: $info_segmenta"  >> segmentaciones.log');
         $processLog->run(null, ['info_segmenta' => " Aglomerado: ".$aglo ." Radio ".$radio]);
-
 
         $esquema = 'e'.$aglo;
 
         // Ejemplo: python3 app/developer_docs/segmentacion-core/lados_completos/lados_completos.py e0777.arc 50 084 1 4 20 30 10 1 
         $process = Process::fromShellCommandline('/usr/bin/python3 ../app/developer_docs/segmentacion-core/lados_completos/lados_completos.py $tabla $prov $dpto $frac $rad $deseada $max $min $indivisible'); 
         $process->setTimeout(3600);
+//        $process->disableOutput();
+//        dd($process);
         
         $process->run(null, ['tabla' => $esquema.".arc",'prov'=>$prov,'dpto'=>$dpto,'frac'=>$frac,'rad'=>$radio,
                              'deseada'=>$vivs_deseada,'max'=>$vivs_max,'min'=>$vivs_min,'indivisible'=>$mza_indivisible]);
                         // executes after the command finishes
+        //dd($process->getErrorOutput());
                         if (!$process->isSuccessful()) {
-                                dd($process);
+                                dd($process->getErrorOutput());
                         }else{  
-  return                          $this->resultado=$process->getOutput();
-//                            dd( $this->resultado );
+                            return $this->resultado=$process->getOutput();
                         }
             // e0777.arc 50 084 1 4 20 30 10 1');
      }
