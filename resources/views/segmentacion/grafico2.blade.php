22c9ff229f4309fa05b791121048830b0af84de1
Manuel Retamozo <manuel@retamozo.com.ar>
2020-03-05 16:05:52 -0300

    Resumen de segmentación en gráficos

--- resources/views/segmentacion/grafico2.blade.php
+++ resources/views/segmentacion/grafico2.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
+<div class="text-center">
+<h2 class="text-center">Segmentación a manzanas independientes</h2>
 <h3 class="text-center">Aglomerado ({{ $aglomerado->codigo }}) {{ $aglomerado->nombre }}</h3>
-<canvas id="canvas" style="padding: 20px;" height="280" width="600"></canvas>
+<div id ="resumen"></div>
+<canvas id="canvas" style="padding: 20px 50px 20px 50px; max-height: 600px; " height="280" width="600"></canvas>
+</div>
 @endsection
 @section('footer_scripts')
        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
@@ -12,10 +16,15 @@
         var Viviendas = new Array();
         $(document).ready(function(){
           $.post(url, {"_token": "{{ csrf_token() }}"},function(response){
+            var sum = 0;
+            var n_segs= response.length;
             response.forEach(function(data){
                 SegmentosCantidad.push(data.cant_segmentos);
                 Viviendas.push(data.vivs);
+                sum += Number(data.vivs);
             });
+            var mensaje = n_segs+' segmentos para '+sum+' viviendas, con un promedio de '+sum/n_segs+' viviendas x segmento';
+            document.getElementById("resumen").innerHTML=mensaje;
             var ctx = document.getElementById("canvas").getContext('2d');
                 var myChart = new Chart(ctx, {
                   type: 'bar',
--- resources/views/segmentacion/grafico2_lados.blade.php
+++ resources/views/segmentacion/grafico2_lados.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
+<div class="text-center">
+<h2 class="text-center">Segmentación a lados completos</h2>
 <h3 class="text-center">Aglomerado ({{ $aglomerado->codigo }}) {{ $aglomerado->nombre }}</h3>
-<canvas id="canvas" style="padding: 20px;" height="280" width="600"></canvas>
+<div id ="resumen"></div>
+<canvas id="canvas" style="padding: 20px 50px 20px 50px; max-height: 600px; " height="280" width="600"></canvas>
+</div>
 @endsection
 @section('footer_scripts')
        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
@@ -12,15 +16,21 @@
         var Viviendas = new Array();
         $(document).ready(function(){
           $.post(url, {"_token": "{{ csrf_token() }}"},function(response){
+            var sum = 0;
+            var n_segs= response.length;
             response.forEach(function(data){
                 SegmentosCantidad.push(data.cant_segmentos);
                 Viviendas.push(data.vivs);
+                sum += Number(data.vivs);
             });
+            var mensaje = n_segs+' segmentos para '+sum+' viviendas, con un promedio de '+sum/n_segs+' viviendas x segmento';
+            document.getElementById("resumen").innerHTML=mensaje;
+            
             var ctx = document.getElementById("canvas").getContext('2d');
                 var myChart = new Chart(ctx, {
                   type: 'bar',
                   data: {
-                      labels:Viviendas,
+                      labels: Viviendas,
                       datasets: [{
                           label: 'Número de Segmentos',
                           data: SegmentosCantidad,
@@ -30,13 +40,19 @@
                       }]
                   },
                   options: {
+                      tooltips: {
+                        callbacks: {
+                            title: function(tooltipItem, data) { return 'Cantidad de Viviendas: '+tooltipItem[0].xLabel }
+                        }
+                       },
                       responsive: true,
                       scales: {
-                          yAxes: [{
+                          yAxes: [
+                              {
                               gridLines: {
                                   drawBorder: true,
                                   color: ['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
-                        }     ,
+                              },
                               ticks: {
                                   beginAtZero:true
                               }
