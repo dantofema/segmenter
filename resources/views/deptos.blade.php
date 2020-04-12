48ceb707aee9bd8ab07976807c7d44f53e90967f
Hernán Alperin <hernan.alperin@gmail.com>
2020-04-12 05:58:06 -0300

    otro <br> out

--- resources/views/deptos.blade.php
+++ resources/views/deptos.blade.php
@@ -58,7 +58,7 @@
              <th>Id</th>
              <th>Código</th>
              <th>Nombre</th>
-             <th>Cantidsd de</br> Localidades</th>
+             <th>Cantidsd de Localidades</th>
           </tr>
        </thead>
     </table>
