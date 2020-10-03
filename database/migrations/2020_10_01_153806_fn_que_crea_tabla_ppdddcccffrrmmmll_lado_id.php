30de8b3992639fe0c7d09b2e91389126bf6a05fd
Hernán Alperin <alpe@alum.mit.edu>
2020-10-03 07:14:21 -0300

    identifica los lados

--- database/migrations/2020_10_01_153806_fn_que_crea_tabla_ppdddcccffrrmmmll_lado_id.php
+++ database/migrations/2020_10_01_153806_fn_que_crea_tabla_ppdddcccffrrmmmll_lado_id.php
@@ -17,9 +17,12 @@ class FnQueCreaTablaPpdddcccffrrmmmllLadoId extends Migration
         $path = 'app/developer_docs/Segmentacion-CORE/lados_completos/cargar_conteos.sql';
         DB::unprepared(file_get_contents($path));
         // fix bug diferente nombres de campos
+        $path = 'app/developer_docs/Segmentacion-CORE/lados_completos/cargar_lados.sql';
+        DB::unprepared(file_get_contents($path));
+        // crea tabla con ids integer de lados ppdddcccffrrmmmll
         $path = 'app/developer_docs/Segmentacion-CORE/lados_completos/segmentos_lados_desde_hasta.sql';
         DB::unprepared(file_get_contents($path));
-        // fix crea tabla para seguir un segemto desde hasta y por los lados que pasa
+        // crea tabla para seguir un segemto desde hasta y por los lados que pasa
 
     }
 
