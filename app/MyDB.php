9d1173f6cbc360930fd124cc5ab19bbdbb0fe6d4
Retamozo Manuel <mretamozo@indec.gob.ar>
2020-12-10 13:35:18 -0300

    CAmbios en grafo, agrego detalle con radios por cantidad de viviendas

--- app/MyDB.php
+++ app/MyDB.php
@@ -312,7 +312,7 @@ FROM
 	{
         $esquema = 'e'.$esquema;
         if (Schema::hasTable($esquema.'.segmentos_desde_hasta')){
-        	return DB::select("SELECT segmento_id, frac, radio, mza , lado , 
+           return DB::select("SELECT segmento_id, frac, radio, mza, lado,
                         CASE  WHEN completo THEN 'Lado Completo'
                         ELSE 'Desde ' ||
                         indec.descripcion_domicilio('".$esquema."',seg_lado_desde) || '
@@ -343,8 +343,11 @@ FROM
     public static function segmentar_equilibrado_ver_resumen($esquema)
 	{
         $esquema = 'e'.$esquema;
-    	return DB::select('SELECT vivs,count(*) cant_segmentos FROM (
-                        SELECT segmento_id,count(*) vivs,count(distinct mza) as mzas,array_agg(distinct prov||dpto||codloc||frac||radio||mza||lado),count(distinct lado) as lados FROM 
+        return DB::select('SELECT vivs,count(*) cant_segmentos,
+                        string_agg(distinct array_to_string(en_lados,\' \'),\',\') en_lados FROM (
+                        SELECT segmento_id,count(*) vivs,count(distinct mza) as
+                        mzas,array_agg(distinct
+                        frac||radio) en_lados,count(distinct lado) as lados FROM
                         '.$esquema.'.
                         segmentacion s JOIN 
                         '.$esquema.'.
@@ -517,9 +520,9 @@ and
     public static function getNodos($esquema,$radio = '%01103')
     {
         return DB::select('SELECT distinct *, substr(mza_i,13,3)||\':\'||lado_i as label,c.conteo FROM (
-                                            SELECT mza_i,lado_i from e'.$esquema.'.lados_adyacentes WHERE mza_i like :radio UNION 
+                                            SELECT mza_i,lado_i from e'.$esquema.'.lados_adyacentes WHERE mza_i like :radio UNION
                                             SELECT mza_j,lado_j from e'.$esquema.'.lados_adyacentes WHERE mza_j like :radio) foo
-LEFT JOIN 
+    LEFT JOIN
     e'.$esquema.'.conteos c
     ON (c.prov,c.dpto,c.codloc,c.frac,c.radio,c.mza,c.lado)=
         (substr(mza_i,1,2)::integer,
@@ -542,19 +545,19 @@ LEFT JOIN
     public static function getSegmentos($esquema,$radio = '%01103')
     {
         if (Schema::hasTable('e'.$esquema.'.arc')) {
-                return DB::select('SELECT array_agg(mza||\'-\'||lado) segmento 
-FROM
-(SELECT 
-mzai mza,ladoi lado, segi seg
-FROM e'.$esquema.'.arc
-UNION
- SELECT
-mzad mza,ladod lado, segd seg
-FROM e'.$esquema.'.arc
-) segs
-WHERE mza like :radio
-GROUP BY seg
-;',['radio'=>$radio.'%']);
+                return DB::select('SELECT array_agg(mza||\'-\'||lado) segmento
+                                    FROM
+                                    (SELECT
+                                        mzai mza,ladoi lado, segi seg
+                                        FROM e'.$esquema.'.arc
+                                    UNION
+                                         SELECT
+                                        mzad mza,ladod lado, segd seg
+                                        FROM e'.$esquema.'.arc
+                                    ) segs
+                                    WHERE mza like :radio
+                                    GROUP BY seg
+                                    ;',['radio'=>$radio.'%']);
         }else{
             return null;
         }
--- resources/views/segmentacion/grafico2.blade.php
+++ resources/views/segmentacion/grafico2.blade.php
@@ -10,10 +10,12 @@
 @section('footer_scripts')
        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
         <script>
+        var myChart = '';
         var url = "{{url('ver-segmentacion-grafico-resumen')}}/{{$aglomerado->id}}";
         var SegmentosCantidad = new Array();
         var Labels = new Array();
         var Viviendas = new Array();
+        var Detalle = new Array();
         $(document).ready(function(){
           $.post(url, {"_token": "{{ csrf_token() }}"},function(response){
             var sum = 0;
@@ -23,6 +25,7 @@
                 Viviendas.push(data.vivs);
                 sum += Number(data.vivs)*Number(data.cant_segmentos);
                 n_segs += Number(data.cant_segmentos);
+                Detalle.push(data.en_lados);
             });
             var mensaje = n_segs+' segmentos para '+sum+' viviendas, con un promedio de '+Math.round (100*sum/n_segs)/100+' viviendas x segmento';
             document.getElementById("resumen").innerHTML=mensaje;
@@ -32,8 +35,8 @@
                   data: {
                       labels:Viviendas,
                       datasets: [{
-                          label: 'Número de Segmentos',
-                          data: SegmentosCantidad ,
+                          label: 'Número de Segmentos ',
+                          data: SegmentosCantidad,
                           borderWidth: 1,
                           backgroundColor: 'rgb(36, 125, 173)',
                           borderColor: 'rgb(66, 155, 213)'
@@ -41,13 +44,14 @@
                   },
                   options: {
                       responsive: true,
+                      borderRadius: 10,
                       scales: {
                           yAxes: [{
                               gridLines: {
                                   title: 'Cantidad de segmentos',
                                   drawBorder: true,
                                   color: ['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
-                        }     ,
+                              },
                               ticks: {
                                   beginAtZero:true
                               }
