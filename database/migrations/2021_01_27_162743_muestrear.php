efc65a38d000ef2bde82e21d6002229f7e53d210
Retamozo Manuel <mretamozo@indec.gob.ar>
2021-01-27 20:50:57 -0300

    Funcion de muestreo

---
+++ database/migrations/2021_01_27_162743_muestrear.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Muestrear extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $path = 'app/developer_docs/segmentacion-core/muestreo/muestrear.sql';
+        DB::unprepared(file_get_contents($path));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
