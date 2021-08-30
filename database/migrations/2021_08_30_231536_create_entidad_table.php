7121527025d89f1109e12baff4d8f750b2fc575b
Hernán Alperin <alpe@alum.mit.edu>
2021-08-30 20:27:47 -0300

    migrate & seed entidad ya estaban dumpeados en app/dev

---
+++ database/migrations/2021_08_30_231536_create_entidad_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntidadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+/*        Schema::create('entidad', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+*/
+       $sql = file_get_contents(app_path() . '/developer_docs/entidad.up.sql');
+       DB::unprepared($sql);        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entidad');
+    }
+}
