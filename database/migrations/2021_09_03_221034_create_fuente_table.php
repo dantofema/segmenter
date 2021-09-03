07c208be9315a6732d93cc8a526cb69ad7f78ff7
Hernán Alperin <alpe@alum.mit.edu>
2021-09-03 19:27:08 -0300

    agrega tabla fuente a DB

---
+++ database/migrations/2021_09_03_221034_create_fuente_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuenteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+/*        Schema::create('fuente', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+*/
+       $sql = file_get_contents(app_path() . '/developer_docs/fuente.up.sql');
+       try{
+           DB::unprepared($sql);
+       }catch(Illuminate\Database\QueryException $e){
+          DB::Rollback();
+          echo _('Error creando tablas fuente ...').$e;
+       }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fuente');
+    }
+}
