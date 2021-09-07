58eccf4c0e7526b9dd42947cd1574ff02241ac5b
Manuel Retamozo <manuel@retamozo.com.ar>
2021-09-06 21:22:35 -0300

    class container, en view y no en popup para depto y prov info

--- resources/views/deptoview.blade.php
+++ resources/views/deptoview.blade.php
@@ -3,5 +3,7 @@
 @section('title', '{{ $departamento->parent->nombre ?? ""}} Departamento: {{ $departamento->nombre }}')
 
 @section('content')
+<div class=container >
      @include('deptoinfo')
+</div>
 @stop
