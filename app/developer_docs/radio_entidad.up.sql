6437f4b872d265a69e2897709d4b2ef3595e9784
Hernán Alperin <alpe@alum.mit.edu>
2021-10-13 20:21:34 -0300

    agrega relacion radio-entidad

---
+++ app/developer_docs/radio_entidad.up.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2 (Ubuntu 12.2-4)
+-- Dumped by pg_dump version 12.2 (Ubuntu 12.2-4)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', 'public', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: radio_entidad; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.radio_entidad (
+    radio_id integer,
+    entidad_id integer
+);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
