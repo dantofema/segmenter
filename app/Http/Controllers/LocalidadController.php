8eb9021e9792f6c246cbfc3669a03af093aacb17
Hernán Alperin <alpe@alum.mit.edu>
2021-12-04 15:27:36 -0300

    $carto y $listado del controler Localidad apuntan a los atributos del Model Localidad

--- app/Http/Controllers/LocalidadController.php
+++ app/Http/Controllers/LocalidadController.php
@@ -65,7 +65,8 @@ class LocalidadController extends Controller
                         'localidad'=>$localidad,
                         'aglomerado'=>$localidad->aglomerado,
                         'radios'=>$localidad->radios,
-                        'carto'=>null,'listado'=>null
+                        'carto'=>$localidad->carto,
+                        'listado'=>$localidad->listado
                         ]);
     }
 
