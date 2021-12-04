12fa02cfdd462fe2b4e5a8c40d0024340c144417
Hernán Alperin <alpe@alum.mit.edu>
2021-12-03 21:53:24 -0300

    pasa a Localidad las cuestiones de segmentacion que estan en Aglo para el cambio de eAAAA a sPPDDDLLL en el nombre del esquema 	modified:   app/Http/Controllers/LocalidadController.php 	modified:   app/Model/Localidad.php 	new file:   resources/views/localidad/segmenta.blade.php 	modified:   routes/web.php

--- app/Model/Localidad.php
+++ app/Model/Localidad.php
@@ -3,6 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class Localidad extends Model
 {
@@ -16,6 +19,11 @@ class Localidad extends Model
     // Sin fecha de creación o modificación
     //
     public $timestamps = false;
+    public $carto;
+    public $listado;
+    public $segmentadoListado;
+    public $segmentadoLados;
+
 
     /**
      * Relación con Departamento, una localidad puede estar en muchos departamentos (caso CABA).
@@ -48,7 +56,247 @@ class Localidad extends Model
         return $codloc= substr(trim($this->codigo), 5, 3);
     }
 
+    public function getCartoAttribute($value)
+    {
+        if(! $this->carto) {
+            if (Schema::hasTable('e'.$this->codigo.'.arc')) {
+                $this->carto = true;
+            }else{
+                $this->carto = false;
+            }
+        }
+        return $this->carto;
+    }
     
+
+    public function getListadoAttribute($value)
+    {
+        /// do your magic
+        if (! $this->listado) {
+
+            if (Schema::hasTable('e'.$this->codigo.'.listado')) {
+                //
+                $this->listado= true;
+            }else{
+                $this->listado= false;
+            }
+        }
+        return $this->listado;
+    }
+
+    public function getSegmentadolistadoAttribute($value)
+    {
+        /// do your magic
+        if (! $this->segmentadoListado) {
+          if (Schema::hasTable('e'.$this->codigo.'.segmentacion')) {
+              return $this->segmentadoListado = true;
+          }else{
+              return $this->segmentadoListado = false;
+          }
+        }
+        return $this->segmentadoListado;
+    }
+
+    public function getSegmentadoladosAttribute($value)
+    {
+        /// do your magic
+        if (! $this->segmentadoLados) {
+          if (Schema::hasTable('e'.$this->codigo.'.arc')) {
+              $radios = DB::table('e'.$this->codigo.'.arc')
+                           ->select(DB::raw("distinct substr(mzai,1,12) link"))
+                           ->whereNotNull('segi')
+                           ->orwhereNotNull('segd');
+              if ($radios->count()>0){
+                  return $this->segmentadoLados = true;
+              }else{
+                  return $this->segmentadoLados = false;
+              }
+          }else{
+              return $this->segmentadoLados = false;
+          }
+        }
+        return $this->segmentadoLados;
+    }
+
+    public function setCartoAtribute()
+    {
+        if (! $this->carto) {
+          if (Schema::hasTable('e'.$this->codigo.'.arc')) {
+              return $this->carto = true;
+          }else{
+              return $this->carto = false;
+          }
+        }
+        return $this->carto;
+
+    }
+
+    public function getRadiosAttribute()
+    {
+        $radios= null;
+        if ($this->Listado==1){
+            $radios = DB::table('e'.$this->codigo.'.listado')
+                                ->select(DB::raw("prov||dpto||frac||radio as link,codloc,
+             '('||dpto||') '||max(nom_dpto)||': '||frac||' '||radio as nombre,
+             count(distinct mza) as cant_mzas,
+             count(*) as registros,
+             count(indec.contar_vivienda(tipoviv)) as vivs,
+             count(CASE WHEN tipoviv='A' THEN 1 else null END) as vivs_a,
+             count(CASE WHEN (tipoviv='B1' or tipoviv='B2') THEN 1 else null END) as vivs_b,
+             count(CASE WHEN tipoviv='CA/CP' THEN 1 else null END) as vivs_c,
+             count(CASE WHEN tipoviv='CO' THEN 1 else null END) as vivs_co,
+             count(CASE WHEN (tipoviv='D'  or tipoviv='J'  or tipoviv='VE' )THEN 1 else null END) as vivs_djve,
+             count(CASE WHEN tipoviv='' THEN 1 else null END) as vivs_unclas
+            "))
+                                ->groupBy('prov','dpto','codloc','frac','radio')
+                                ->orderBy('prov','asc')
+                                ->orderBy('dpto','asc')
+                                ->orderBy('codloc','asc')
+                                ->orderBy('frac','asc')
+                                ->orderBy('radio','asc')
+                                ->get();
+        }
+            $links=[];
+            $new_radios=[];
+            $objRadios= Collect (new Radio);
+            $nuevos_radios=0;
+        if($radios){
+            foreach($radios as $radio){
+                if (Radio::where('codigo',$radio->link)->exists()){
+                               $links[]=$radio->link;
+                }else{
+                    $new_radios[]=new Radio (['codigo'=>$radio->link,'nombre'=>'Nuevo: '.$radio->link]);
+        $nuevos_radios++;
+        flash('No se encontró radio  -> '.$radio->link)->error()->important();
+                }
+            }
+        }
+        if (count($links)>0){
+            $objRadios=Radio::whereIn('codigo',$links)->get();
+      flash('Radios verificados -> '.$objRadios->count())->success();
+        }
+            //dd($new_radios,$nuevos_radios);
+            $objs=$objRadios->union(Collect ($new_radios));
+        return $objs;
+
+    }
+
+    public function getComboRadiosAttribute()
+    {
+        $radios= null;
+        if ($this->Listado==1){
+            $radios = DB::table('e'.$this->codigo.'.listado')
+                                ->select(DB::raw("prov||dpto||frac||radio as link,codloc,
+             '('||dpto||') F'||frac||' R'||radio as nombre,
+             count(distinct mza) as cant_mzas,
+             count(*) as registros,
+             count(indec.contar_vivienda(tipoviv)) as vivs,
+             count(CASE WHEN tipoviv='A' THEN 1 else null END) as vivs_a,
+             count(CASE WHEN (tipoviv='B1' or tipoviv='B2') THEN 1 else null END) as vivs_b,
+             count(CASE WHEN tipoviv='CA/CP' THEN 1 else null END) as vivs_c,
+             count(CASE WHEN tipoviv='CO' THEN 1 else null END) as vivs_co,
+             count(CASE WHEN (tipoviv='D'  or tipoviv='J'  or tipoviv='VE' )THEN 1 else null END) as vivs_djve,
+             count(CASE WHEN tipoviv='' THEN 1 else null END) as vivs_unclas
+    "))
+                                ->groupBy('prov','dpto','codloc','frac','radio')
+                                ->orderBy('prov','asc')
+                                ->orderBy('dpto','asc')
+                                ->orderBy('codloc','asc')
+                                ->orderBy('frac','asc')
+                                ->orderBy('radio','asc')
+                                ->get();
+        }
+        return $radios;
+    }
+
+    public function getSVG()
+    {
+        // return SVG Carto? Listado? Segmentación?
+        // WITH shapes (geom, attribute) AS (a
+//
+//  VALUES
+//    ((SELECT ST_MakeLine(ST_MakePoint(0,0), ST_MakePoint(50,50))), 2),
+        //    ((SELECT ST_Envelope(ST_MakeBox2d(ST_MakePoint(0,0), st_makepoint(10,10)))), 3)
+
+        if ($this->Carto){
+        $height=800;
+        $width=900;
+        $escalar=false;
+            $extent=DB::select("SELECT box2d(st_collect(wkb_geometry)) box FROM  e".$this->codigo.".arc");
+            $extent=$extent[0]->box;
+            list($x0,$y0,$x1,$y1) = sscanf($extent,'BOX(%f %f,%f %f)');
+
+             $Dx=$x1-$x0; $Dy=$y1-$y0;
+            if (!$height and $width) $height=round($width*$Dy/$Dx);
+            if ($height and !$width) $width=round($height*$Dx/$Dy);
+            if (!$height and !$width) {$width=round($perimeter*$Dx/2/($Dx+$Dy)); $height=round($width*$Dy/$Dx);}
+            $dx=$Dx/$width; $dy=$Dy/$height; $epsilon=min($dx,$dy)/15; // mínima reolución, acho y alto de lo que representa un pixel
+            if ($escalar) {$viewBox="0 0 $width $height"; $stroke=2;}
+            else { $viewBox=$this->viewBox($extent,$epsilon,$height,$width); $stroke=2*$epsilon;
+        }
+
+            //dd($viewBox.'/n'.$this->viewBox($extent,$epsilon,$height,$width).'/n'.$x0." -".$y0." ".$x1." -".$y1);
+            $svg=DB::select("
+WITH shapes (geom, attribute) AS (
+    ( SELECT st_buffer(ST_OffsetCurve(wkb_geometry,5),2) wkb_geometry, segi
+    FROM e".$this->codigo.".arc WHERE segi is not null)
+    UNION (
+    SELECT st_buffer(ST_OffsetCurve(wkb_geometry,-5),2) wkb_geometry, segd
+    FROM e".$this->codigo.".arc WHERE segd is not null)
+    UNION (
+    SELECT st_buffer(ST_OffsetCurve(wkb_geometry,-5),2) wkb_geometry, 0
+    FROM e".$this->codigo.".arc WHERE segd is null)
+    UNION (
+    SELECT st_buffer(ST_OffsetCurve(wkb_geometry,5),2) wkb_geometry, 0
+    FROM e".$this->codigo.".arc WHERE segi is null)
+  ),
+  paths (svg) as (
+     SELECT concat(
+         '<path d= \"',
+         ST_AsSVG(st_buffer(st_union(geom),5),0), '\" ',
+         CASE WHEN attribute = 0 THEN 'stroke=\"gray\" stroke-width=\"2\"
+         fill=\"gray\"'
+              WHEN attribute < 5 THEN 'stroke=\"none\"
+              stroke-width=\"".$stroke."\" fill=\"#' || attribute*20 || 'AAAA\"'
+              WHEN attribute < 10 THEN 'stroke=\"none\"
+         stroke-width=\"".$stroke."\" fill=\"#00' || (attribute-5)*20 || '00\"'
+              WHEN attribute < 15 THEN 'stroke=\"none\"
+         stroke-width=\"".$stroke."\" fill=\"#AA' || (attribute-10)*20 || '00\"'
+         ELSE
+            'stroke=\"black\" stroke-width=\"".$stroke."\" fill=\"#22' ||
+            attribute*10 || '88\"'
+         END,
+          ' />')
+     FROM shapes GROUP BY attribute
+ )
+ SELECT concat(
+         '<svg id=\"aglo_".$this->codigo."\"xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"".$viewBox.
+         "\" height=\"".$height."\" width=\"".$width."\">',
+         array_to_string(array_agg(svg),''),
+         '</svg>')
+ FROM paths;
+");
+
+            return $svg[0]->concat;
+        }else{ return "No geodata"; }
+
+    }
+
+    private function viewBox($extent,$epsilon,$height,$width){
+        list ( $x0, $y0, $x1, $y1 ) = sscanf ( $extent, 'BOX(%f %f,%f %f)' );
+        $Dx = $x1 - $x0;
+        $Dy = $y1 - $y0;
+            $m_izq=.1*$Dx; $m_der=.1*$Dx; $m_arr=.1*$Dy; $m_aba=.1*$Dy;
+        $viewBox = ($x0 - $m_izq) . " " . (- $y1 - $m_arr) . " " . ($Dx + $m_izq + $m_der) . " " . ($Dy + $m_arr + $m_aba);
+        if (! $height and ! $width)
+            $height = 600;
+        if (! $height)
+            $height = $width * $Dy / $Dx;
+        if (! $width)
+           $width = $height * $Dx / $Dy;
+        $epsilon = min ( $Dx / $width, $Dy / $height );
+        return $viewBox;
+    }
 
 
 }
---
+++ resources/views/localidad/segmenta.blade.php
@@ -0,0 +1,74 @@
+<div class="container">
+    Información de la localidad ({{ $localidad->codigo }}) 
+    <b> {{ $localidad->nombre }} </b><br />
+    <p>
+     @if($carto)
+        La base geográfica está cargada.
+     @else
+        NO está cargada la base geográfica.
+     @endif 
+    <br />
+     @if($listado)
+        El listado de viviendas está cargado.
+     @else
+        NO está cargado el listado de viviendas.
+     @endif 
+    <br />
+    </p>
+<div class="form-horizontal">
+<form action="/localidad-segmenta-run/{{ $localidad->id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+  <div class="form-group">
+    <label class="control-label" for="radio">Seleccione el Radio a segmentar:</label>
+    <div class="">
+        <select name="radios" class="form-control" >
+            @foreach($radios as $radio)
+                <option value="{{ $radio->link }}">{{ trim($radio->nombre) }} - Viv: {{ trim($radio->vivs) }}</option>
+            @endforeach
+        </select>
+    </div>
+  </div>
+  <div class="form-group" style="display: none; visibility: hidden;"> 
+    <label class="form-check-label">
+      <input name=checkallradios type="checkbox" class="form-check-input"
+      value=allradios>
+          Todos los radios
+    </label>
+  </div>  
+  <div class="form-group">
+    <label class="control-label" for="radio">Método de segmentación:</label><br />
+    <label class="radio-inline"><input type="radio" name="optalgoritmo" value=listado>Manzanas independientes</label>
+    <label class="radio-inline"><input type="radio" name="optalgoritmo" value=lados>Lados Completos</label>
+    <label class="radio-inline" title="Primero segmenta a lados completos, luego los lados excedidos son segmentados a lado independiente."
+    alt="Primero segmenta a lados completos, luego los lados excedidos son segmentados a lado independiente."><input type="radio" name="optalgoritmo"
+    value=magic checked>Mixto</label>
+  </div>
+  <div class="form-group">
+    <label class="control-label" for="radio">Parametros:</label><br />
+    <div class="">
+     <label class="control-label " for="radio">Cantidad deseada de viviendas:</label>
+     <input id="vivs_deaseadas" type="integer" maxlength=3 size=3
+     name="vivs_deseadas" value="36"><br />
+    </div>
+    <div class="">
+     <label class="control-label " for="radio">Cantidad máxima deseada:</label>
+     <input id="vivs_max" type="integer" maxlength=3 size=3 name="vivs_max"
+     value="36"><br />
+    </div>
+    <div class="">
+     <label class="control-label " for="radio">Cantidad Mínima deseada:</label>
+     <input id="vivs_min" type="integer" maxlength=3 size=3 name="vivs_min"
+     value="28"><br />
+    </div>
+    <div class="">
+     <label class="control-label " for="radio">Mantener manzana indivisible con menos de:</label>
+     <input id="mzas_indivisibles" type="integer" maxlength=3 size=3
+     name="mzas_indivisibles" value="5"> viviendas
+    </div>
+  </div>
+  <div class="mx-auto">
+   <input type="submit" class="segmentar btn btn-primary" value="Segmentar">
+  </div>
+</form>
+</div>
+</div>
