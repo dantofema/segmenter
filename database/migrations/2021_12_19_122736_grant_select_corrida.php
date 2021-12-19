8fc5dd6bc1d3e4f34dbcbfb8e3249b924755a17d
Hernán Alperin <alpe@alum.mit.edu>
2021-12-19 12:33:02 -0300

    permisos de lectura a tabla corrida a geoestadistica_admin

---
+++ database/migrations/2021_12_19_122736_grant_select_corrida.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GrantSelectCorrida extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        try{
+            DB::statement("grant select on table public.corrida to geoestadistica_admin;");
+        }catch(QueryException $e){
+                Log::error('No se pudo dar permisos de lectura a tabla corrida a geoestadistica_admin');
+            }
+            Log::debug('Se dieron permisos de lectura a tabla corrida a geoestadistica_admin');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
