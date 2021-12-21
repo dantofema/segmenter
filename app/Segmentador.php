55283cd34338e09a795d9dcb6d3ae74d3f3d1a98
Manuel Retamozo <manuel@retamozo.com.ar>
2021-12-21 15:20:07 -0300

    Agrego Facade log para error segmentación python

--- app/Segmentador.php
+++ app/Segmentador.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Symfony\Component\Process\Process;
