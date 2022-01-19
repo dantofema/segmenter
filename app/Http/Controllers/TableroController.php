55a7990da9687ea2b2634841a538bd9ffd916d66
Manuel Retamozo <manuel@retamozo.com.ar>
2022-01-19 18:43:25 -0300

    quito grafico de area

--- app/Http/Controllers/TableroController.php
+++ app/Http/Controllers/TableroController.php
@@ -41,7 +41,8 @@ class TableroController extends Controller
              $data = json_encode ($avances);
              return response()->json($avances);
          }else{
-             return view('grafico.show',['titulo'=>$titulo,'url_data'=>'avance','tipo_grafico'=>'No-area']);
+             return view('grafico.show',['titulo'=>$titulo,'url_data'=>'avance']);
+//,'tipo_grafico'=>'No-area']);
          }
     }
 }
