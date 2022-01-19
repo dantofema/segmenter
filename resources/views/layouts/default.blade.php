b9e6938fa8f8004693b8191dc492713236c01b50
Manuel Retamozo <manuel@retamozo.com.ar>
2022-01-18 21:44:14 -0300

    copyright 2022, sólo simbolo como pagina de indec

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -99,7 +99,7 @@
         </div>
             @yield('content_main')
         <div id="copyright" class="text-center justify-content-center"
-            style="display:block"><hr />© Copyright 2021 INDEC - Geoestadística
+            style="display:block"><hr />© 2022 INDEC - Geoestadística
             <div>{{ Git::branch() }} - {{ Git::version() }} -  {{ Git::lastCommitDate() }}</div>
             </div>
 <!-- If using flash()->important() or flash()->overlay(), you'll need to pull in the JS for Twitter Bootstrap. -->
