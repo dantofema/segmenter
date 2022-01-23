d8992693c1bed1364beaa7af650dde2ce121d5f3
Manuel Retamozo <manuel@retamozo.com.ar>
2022-01-23 17:37:11 -0300

    Titulo y subtitulo

--- app/Http/Controllers/TableroController.php
+++ app/Http/Controllers/TableroController.php
@@ -10,13 +10,14 @@ class TableroController extends Controller
     // Primer tablero de informe por provincias.
     // Histograma radios segmentados.
     public function GraficoProvincias(Request $request) {
-        $titulo = "Actividad diaria: cantidad de radios\n según última fecha de radio segmentado";
+        $titulo = "Actividad diaria";
+        $subtitulo = "Cantidad de radios según última fecha de radio segmentado";
         if ($request->isMethod('post')) {
              $avances = MyDB::getAvancesProv();
              $data = json_encode ($avances);
              return response()->json($avances);
          }else{
-             return view('grafico.show',['titulo'=>$titulo,'url_data'=>'prov']);
+             return view('grafico.show',['titulo'=>$titulo,'subtitulo'=>$subtitulo,'url_data'=>'prov']);
          }
     }
  
@@ -35,13 +36,14 @@ class TableroController extends Controller
     // Terecer tablero de informe por provincias.
     // Histograma radios segmentadosi acumulado.
     public function GraficoAvance(Request $request) {
-        $titulo = "Avance de segmentación por provincia: cantidad de radios\n según última fecha de radio segmentado";
+        $titulo = "Avance de segmentación por provincia";
+        $subtitulo = "Cantidad de radios según última fecha de radio segmentado";
         if ($request->isMethod('post')) {
              $avances = MyDB::getAvanceProvAcum();
              $data = json_encode ($avances);
              return response()->json($avances);
          }else{
-             return view('grafico.show',['titulo'=>$titulo,'url_data'=>'avance']);
+             return view('grafico.show',['titulo'=>$titulo,'subtitulo'=>$subtitulo,'url_data'=>'avance']);
 //,'tipo_grafico'=>'No-area']);
          }
     }
