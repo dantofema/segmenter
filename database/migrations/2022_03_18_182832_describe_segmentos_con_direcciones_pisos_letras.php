4773716ca5e1fc6e6c351463fe3528698732731b
Manuel Retamozo <manuel@retamozo.com.ar>
2022-03-18 18:36:12 -0300

    Acepta pisos con letras

---
+++ database/migrations/2022_03_18_182832_describe_segmentos_con_direcciones_pisos_letras.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DescribeSegmentosConDireccionesPisoLetras extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $path = 'app/developer_docs/segmentacion-core/descripcion_segmentos/describe_sin_muestreo_ffrr.sql';
+        DB::unprepared(file_get_contents($path));
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
