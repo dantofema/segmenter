c9b8548a2e7200c065e9f03124037d828a337845
Manuel Retamozo <manuel@retamozo.com.ar>
2022-03-18 18:37:02 -0300

    Actualiza describe_sin_muestreo

--- database/migrations/2022_03_18_182832_describe_segmentos_con_direcciones_pisos_letras.php
+++ database/migrations/2022_03_18_182832_describe_segmentos_con_direcciones_pisos_letras.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DescribeSegmentosConDireccionesPisoLetras extends Migration
+class DescribeSegmentosConDireccionesPisosLetras extends Migration
 {
     /**
      * Run the migrations.
