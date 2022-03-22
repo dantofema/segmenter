1227ce26f991043e76501ac102403c2e3886afa3
Manuel Retamozo <manuel@retamozo.com.ar>
2022-03-21 23:57:27 -0300

    Recupera funcion indec.segmentar_equilibrado(text,integer)

---
+++ database/migrations/2022_03_21_235432_recreacion_segmentar_equilibrado_recuperar.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RecreacionSegmentarEquilibradoRecuperar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Borro primero si existe por un tema de cambio de nombre de parametro
+        // no permitido
+        $query = 'DROP FUNCTION if exists
+        indec.segmentar_equilibrado(text,integer);';
+        Eloquent::unguard();
+        DB::statement($query);
+
+        //
+        $path = 'app/developer_docs/segmentacion-core/manzanas_independientes/segmentar_equilibrado.sql';
+        DB::unprepared(file_get_contents($path));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+    }
+}
