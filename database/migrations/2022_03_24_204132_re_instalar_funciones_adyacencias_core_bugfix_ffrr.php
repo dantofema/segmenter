49b2956c34d408f08f98433f8455f8f47f5f3313
Manuel Retamozo <manuel@retamozo.com.ar>
2022-03-24 20:57:14 -0300

    24M Nunca mas

--- database/migrations/2022_03_24_204132_re_instalar_funciones_adyacencias_core_bugfix_ffrr.php
+++ database/migrations/2022_03_24_204132_re_instalar_funciones_adyacencias_core_bugfix_ffrr.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ReInstalarFuncionesAdyacenciasCoreBugfix_Ffrr extends Migration
+class ReInstalarFuncionesAdyacenciasCoreBugfixFfrr extends Migration
 {
     /**
      * Run the migrations.
