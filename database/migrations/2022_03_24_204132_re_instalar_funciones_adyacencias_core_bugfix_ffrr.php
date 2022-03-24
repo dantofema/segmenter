e8ad66d786b15bb44f53f13b958db5c1c6491662
Manuel Retamozo <manuel@retamozo.com.ar>
2022-03-24 20:46:29 -0300

    24M Nunca mas

---
+++ database/migrations/2022_03_24_204132_re_instalar_funciones_adyacencias_core_bugfix_ffrr.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ReInstalarFuncionesAdyacenciasCoreBugfix_Ffrr extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Eloquent::unguard();
+        DB::statement('CREATE SCHEMA IF NOT EXISTS indec');
+        //$this->command->info('- Instalando funcion de adyacencias...');
+        $path = 'app/developer_docs/segmentacion-core/lados_completos/generar_adyacencias.sql';
+        DB::unprepared(file_get_contents($path));
+        //$this->command->info('Generar Adyacencias instalado!');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
