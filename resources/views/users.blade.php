df57629bf93b67799a6aaaa3bdcd11938036ae19
Manuel Retamozo <manuel@retamozo.com.ar>
2022-09-21 12:34:05 -0300

    elimino librerias externas

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,9 +3,6 @@
 @section('title', 'Usuarios')
 
 @section('content')
-<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
-
 <div class="container">
 	<div class="row justify-content-center">
     <div class="card">
