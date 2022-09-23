ee274891385673e01254055dadaff8eb3c85adf7
gerov12 <gerogv612@gmail.com>
2022-09-23 12:03:38 -0300

    mensajes informativos en los seeders

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -15,11 +15,14 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {   
+        $this->command->info('Creando permisos del Super Admin...');
         try{
             $asignarRoles = Permission::create(['name' => 'Asignar Roles'],'asignador');
             $quitarRoles = Permission::create(['name' => 'Quitar Roles'],'vetador');
 
+            $this->command->info('Creando rol Super Admin y asignando permisos...');
             $superAdmin = Role::create(['name' => 'Super Admin'])->syncPermissions([$asignarRoles, $quitarRoles]);
+            $this->command->info('Rol Super Admin creado.');
         } catch ( Spatie\Permission\Exceptions $e) {
             echo _($e->getMessage());
         }
