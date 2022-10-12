35e54d945d616e58771ee9be1588c47d37326a0a
Manuel Retamozo <manuel@retamozo.com.ar>
2022-10-12 12:44:43 -0300

    Submodule status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,6 +108,8 @@
         <div id="copyright" class="text-center justify-content-center"
             style="display:block"><hr />© 2022 INDEC - Geoestadística
             <div>{{ Git::branch() }} - {{ Git::version() }} -  {{ Git::lastCommitDate() }}</div>
+            <div>{{ Git::submoduleStatus() }}</div>
+
             </div>
 <!-- If using flash()->important() or flash()->overlay(), you'll need to pull in the JS for Twitter Bootstrap. -->
 <!--script src="//code.jquery.com/jquery.js"></script>
