3aa6a0c4076a72d9b63749a88909253c1397c443
Manuel Retamozo <manuel@retamozo.com.ar>
2022-10-31 13:58:13 -0300

    bugfix 3 #510 Role Permission no catch exceptions

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -18,17 +18,23 @@ class RoleSeeder extends Seeder
     public function run()
     {   
         $this->command->info('Creando permisos del Super Admin...');
+        $allPermissions = Permission::all()->pluck('name')->toArray();
+        $allRoles = Role::all()->pluck('name')->toArray();
         try{
-            if ( Count(Permission::findByName('Asignar Roles')->get()) == 0 )
+            //if ( Count(Permission::findByName('Asignar Roles')->get()) == 0 )
+            if ( !in_array('Asignar Roles', $allPermissions) )
               $asignarRoles = Permission::create(['name' => 'Asignar Roles'],'asignador');
-            if ( Count(Permission::findByName('Quitar Roles')->get()) == 0 )
+
+            if ( !in_array('Quitar Roles', $allPermissions) )
               $quitarRoles = Permission::create(['name' => 'Quitar Roles'],'vetador');
 
             $this->command->info('Creando rol Super Admin y asignando permisos...');
-            if ( Count(Role::findByName('Super Admin')->get()) == 0 ) {
+            if ( !in_array('Super Admin',$allRoles)) {
               $superAdmin = Role::create(['name' => 'Super Admin'])->syncPermissions([$asignarRoles, $quitarRoles]);
               $this->command->info('Rol Super Admin creado.');
             }
+        } catch (      Spatie\Permission\Exceptions\PermissionDoesNotExist $e) {
+            $this->command->info('Permiso no existe...'.$e->getMessage());
         } catch ( Spatie\Permission\Exceptions\PermissionAlreadyExists $e) {
             $this->command->info('Permisos del Super Admin existentes...');
 
