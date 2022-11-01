f18de8621028e970f068d57d19976d2340166e5a
gerov12 <gerogv612@gmail.com>
2022-11-01 11:15:53 -0300

    Chequeo si los permisos existen antes de crearlos

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -17,11 +17,11 @@ class RoleSeeder extends Seeder
     {   
         $this->command->info('Creando permisos del Super Admin...');
         try{
-            $asignarRoles = Permission::create(['name' => 'Asignar Roles'],'asignador');
-            $quitarRoles = Permission::create(['name' => 'Quitar Roles'],'vetador');
+            $asignarRoles = Permission::firstOrcreate(['name' => 'Asignar Roles']);
+            $quitarRoles = Permission::firstOrcreate(['name' => 'Quitar Roles']);
 
             $this->command->info('Creando rol Super Admin y asignando permisos...');
-            $superAdmin = Role::create(['name' => 'Super Admin'])->syncPermissions([$asignarRoles, $quitarRoles]);
+            $superAdmin = Role::firstOrcreate(['name' => 'Super Admin'])->syncPermissions([$asignarRoles, $quitarRoles]);
             $this->command->info('Rol Super Admin creado.');
         } catch ( Spatie\Permission\Exceptions $e) {
             echo _($e->getMessage());
