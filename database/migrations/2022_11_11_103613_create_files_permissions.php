e4047bf3b4dd789fe1c1d467543b815c66e1f0d3
gerov12 <peque_boster@hotmail.com>
2022-11-11 10:42:51 -0300

    Manejo y seeder de permisos sobre archivos #511

---
+++ database/migrations/2022_11_11_103613_create_files_permissions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesPermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call( 'db:seed', [
+            '--class' => 'PermissionSeeder',
+            '--force' => true ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::beginTransaction();
+            /** elimino los permisos */
+            Permission::where(['name'=>'Ver Archivos'])->first()->delete();
+            Permission::where(['name'=>'Administrar Archivos'])->first()->delete();
+            DB::commit();
+    }
+}
