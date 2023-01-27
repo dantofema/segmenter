a29d627dffb61b35c62d13a65f01d26d4abf1a6b
Manuel Retamozo <manuel@retamozo.com.ar>
2023-01-26 21:38:26 -0300

    join arc

--- app/MyDB.php
+++ app/MyDB.php
@@ -1757,7 +1757,7 @@ FROM
                   }
                } else {
                   Log::error('No se pudo cargar la topologia ni utilizando tolerancia de '.$tolerancia.' para '.$esquema .' -> '.$e);
-                  $result = DB::select("SELECT * from indec.crossTopologia('".$esquema."')")->get();
+                  $result = DB::select("SELECT * from indec.crossTopologia('".$esquema."','arc') arc_cross join ".$esquema.".arc a using (ogc_fid)")->get();
                   $cross_result = collect($result)->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
                   flash('Arco con problemas: '.$cross_result)->error()->important();
                }
