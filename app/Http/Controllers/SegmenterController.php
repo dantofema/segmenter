17a7354927881deba27baff84e6e4f4f1458478d
Manuel Retamozo <manuel@retamozo.com.ar>
2023-02-03 17:26:33 -0300

    cambio mensaje, que sigue siendo poco claro

--- app/Http/Controllers/SegmenterController.php
+++ app/Http/Controllers/SegmenterController.php
@@ -134,7 +134,7 @@ class SegmenterController extends Controller
       if( $mensajes=$shp_file->procesar() ) {
         flash('Procesó '.$shp_file->tipo)->important()->success();
         if (isset($shp_lab_file)){
-          flash('2. '.$shp_file->tabla.' == '.$shp_lab_file->tabla);
+          flash('Al mismo esquema temporal lab y arc '.$shp_file->tabla.' == '.$shp_lab_file->tabla);
         }
         $ppdddllls=$shp_file->pasarData();
         flash('3. Se pasó Data de '.count($ppdddllls));
