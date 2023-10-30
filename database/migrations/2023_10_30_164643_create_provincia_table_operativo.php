8af6fa20e012183d26a890cbb47169fc369d95de
Manuel Retamozo <mretamozo@indec.gob.ar>
2023-10-30 17:22:23 -0300

    agreo PK en operativo para FK en tabla provincia_operativo

--- database/migrations/2023_10_30_164643_create_provincia_table_operativo.php
+++ database/migrations/2023_10_30_164643_create_provincia_table_operativo.php
@@ -20,6 +20,14 @@ class CreateProvinciaTableOperativo extends Migration
 	 $sql = file_get_contents(database_path() . '/migrations/2020_01_02_202716_create_provincia_table.up.sql');
 	 DB::unprepared($sql);
 */
+ // Agrego primary key en tabla operativo
+ If (Schema::hasTable('operativo')){
+    Schema::table('operativo', function (Blueprint $table) {
+        $table->primary(id)->change();
+    });
+ 
+}
+
  If (! Schema::hasTable('operativo_provincia')){
     Schema::create('operativo_provincia', function (Blueprint $table) {
         $table->BigInteger('provincia_id')->index()->primary();
