6adb475bf6a337b24daee77ab94410adfc88b710
gerov12 <peque_boster@hotmail.com>
2024-02-21 11:18:26 -0300

    Botones volver a users

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 <div class="container">
+  <div style="display: flex; align-items: center; justify-content: center;">
+    <div style="width: 50rem; display: flex; align-items: center;"> 
+      <h4><a href="{{route('admin.listarUsuarios')}}" class="badge badge-pill badge-primary">← Volver</a></h4>
+    </div>
+  </div>
 	<div class="row justify-content-center">
     <div class="card" style="width: 50rem;">
       <div class="card-header">{{ __('Lista de roles') }} 
